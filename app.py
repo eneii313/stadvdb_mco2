73c2ab34ce604d23a4c5d4c36bac4e3026b2dc32
eneii313 <jonaviene_capunitan@dlsu.edu.ph>
2024-11-30 20:26:50 +0800

    flask init setup

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request
+import pymysql
+
+app = Flask(__name__)
+
+PORT = 3000
+
+# Database Nodes
+HOST = "ccscloud.dlsu.edu.ph"
+PASSWORD = "password"
+DATABASE = "mco2"
+
+nodes = [
+    {"id": 21262, "user": "user1", "online": True, "engine": None},
+    {"id": 21272, "user": "user2", "online": True, "engine": None},
+    {"id": 21282, "user": "user3", "online": True, "engine": None},
+]
+
+# try to connect to a specific node
+def try_connection(node):
+    try:
+        connection = pymysql.connect(
+            host=HOST,
+            port=node["id"],
+            user=node["user"],
+            password=PASSWORD,
+            database=DATABASE
+        )
+        print(f"Connected to node {node['id']}")
+        return connection
+    
+    except Exception as e:
+        print(f"Failed to connect to node {node['id']}: {e}")
+        return None
+
+
+# create connection to all nodes
+def init_connections():
+    for node in nodes:
+        if node["online"]:
+            node["engine"] = try_connection(node)
+
+
+# close all connections
+def close_connections():
+    for node in nodes:
+        if node["engine"]:
+            node["engine"].close()
+            print(f"Connection to node {node['id']} closed.")
+
+
+def fetch_data_from_node(node, query):
+    if node["engine"]:
+        try:
+            with node["engine"].cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+                return result
+        except Exception as e:
+            print(f"Error querying node {node['id']}: {e}")
+    else:
+        print(f"Node {node['id']} is not connected.")
+    return None
+
+
+# -- ROUTES --
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# -- MAIN EXECUTION --
+if __name__ == '__main__':
+    init_connections()
+
+    #sample query
+    query = "SELECT * FROM games LIMIT 10;"
+    data = fetch_data_from_node(nodes[0], query)
+    if data:
+        print(data)
+        
+    app.run(debug=True, port=PORT)
